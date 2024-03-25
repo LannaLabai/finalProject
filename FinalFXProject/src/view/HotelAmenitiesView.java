9fffc67d7dfc5f508ddc9f719eadedeb2e703572
NaghamHalaby <nagham21996@gmail.com>
2024-03-25 16:36:28 +0200

    request&hotelAmenities
    partial changes

--- FinalFXProject/src/view/HotelAmenitiesView.java
+++ FinalFXProject/src/view/HotelAmenitiesView.java
@@ -2,100 +2,109 @@ package view;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-
 import javax.swing.*;
 import javax.swing.border.*;
-
 import utils.ServiceType;
 
 public class HotelAmenitiesView extends BasicViewTemplate {
 
-	private ArrayList<JButton> buttons;
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                HotelAmenitiesView frame = new HotelAmenitiesView();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					HotelAmenitiesView frame = new HotelAmenitiesView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public HotelAmenitiesView() {
+        super("Hotel Amenities");
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public HotelAmenitiesView()  {
-		super("Hotel Amenities");
-	}
-	
-	public HotelAmenitiesView(JFrame nextFrame) {
-		super("Hotel Amenities",nextFrame);
-		
-	}
-	
-	@Override
-	public void initialize() {
-		initializeDefault();
-		
-		contentPane.add(btnBack,BorderLayout.NORTH);
-		
-		lblTitle.setText("<html><h1>Hotel Amenities</h1></html>");
-		lblSubtext.setText("This hotel offers a wide variety of services and amenities. Details listed in each of the following pages:");
-		
-		JPanel btnPanel = new JPanel(new GridLayout(3,3));
-		mainPanel.add(btnPanel);
-		
-		for(ServiceType st: ServiceType.values()) {
-			if(st!=ServiceType.ENTERTAINMENT) {
-				JButton btn = new JButton(st.toString());
-				btnPanel.add(btn);
-				btn.addActionListener(this);
-			}
-		}
-		
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		super.actionPerformed(e);
-		JButton sourceButton = (JButton) e.getSource();
-	    String buttonText = sourceButton.getText();
-	    
-	    if (buttonText.equals(ServiceType.DINING.toString())) {
-	    	new DiningView(this);
-            this.setVisible(false);
-	    } else if (buttonText.equals(ServiceType.SPA.toString())) {
-	    	new SpaView(this);
-            this.setVisible(false);
-	    } else if (buttonText.equals(ServiceType.POOL.toString())) {
-	    	new PoolView(this);
-            this.setVisible(false);
-	    }else if (buttonText.equals(ServiceType.BABYSITTING.toString())) {
-	    	new BabysittingView(this);
-            this.setVisible(false);
-	    } else if (buttonText.equals(ServiceType.GYMACTIVITIES.toString())) {
-	    	new GymActivitiesView(this);
-            this.setVisible(false);
-	    }else if (buttonText.equals(ServiceType.TRIPS.toString())) {
-	    	new TripsView(this);
-            this.setVisible(false);
-	    } else if (buttonText.equals(ServiceType.MANIPEDI.toString())) {
-	    	new ManicureAndPedicureView(this);
-            this.setVisible(false);
-	    }
-	    if(e.getSource()==btnBack) {
-			nextFrame.setVisible(true);
-            this.setVisible(false);
-		}
-	}
+    public HotelAmenitiesView(JFrame nextFrame) {
+        super("Hotel Amenities", nextFrame);
+    }
 
+    @Override
+    public void initialize() {
+        initializeDefault();
+
+        contentPane.add(btnBack, BorderLayout.NORTH);
+
+        lblTitle.setText("<html><h1>Hotel Amenities</h1></html>");
+        lblSubtext.setText("This hotel offers a wide variety of services and amenities. Details listed in each of the following pages.");
+
+        JPanel btnPanel = new JPanel(new GridLayout(0, 3, 5, 5)); // Adjusted layout: 0 rows, 3 columns, with 10px horizontal and vertical gaps
+        mainPanel.add(btnPanel, BorderLayout.CENTER);
+       
+        for (ServiceType st : ServiceType.values()) {
+            if (st != ServiceType.ENTERTAINMENT) {
+                JButton btn = createServiceButton(st.toString());
+                btnPanel.add(btn);
+            }
+        }
+    }
+
+    private JButton createServiceButton(String text) {
+    	JButton btn = new JButton(text);
+        btn.setPreferredSize(new Dimension(50, 30)); // Adjusted size
+        btn.setFont(new Font("Arial", Font.BOLD, 14)); // Reduced font size
+        //btn.setBackground(new Color(255, 204, 102)); // Custom button color
+        btn.setForeground(Color.BLACK);
+        btn.setFocusPainted(false);
+        btn.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2)); // Add border
+        btn.addActionListener(this);
+        return btn;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        super.actionPerformed(e);
+        if (e.getSource() == btnBack) {
+            nextFrame.setVisible(true);
+            this.setVisible(false);
+        } else if (e.getSource() instanceof JButton) {
+            JButton sourceButton = (JButton) e.getSource();
+            String buttonText = sourceButton.getText();
+            openServiceView(buttonText);
+        }
+    }
+
+    private void openServiceView(String service) {
+        switch (ServiceType.valueOf(service)) {
+            case DINING:
+                new DiningView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case SPA:
+                new SpaView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case POOL:
+                new PoolView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case BABYSITTING:
+                new BabysittingView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case GYMACTIVITIES:
+                new GymActivitiesView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case TRIPS:
+                new TripsView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            case MANIPEDI:
+                new ManicureAndPedicureView(this).setVisible(true);
+                this.setVisible(false);
+                break;
+            default:
+                break;
+        }
+    }
 }
+
--- FinalFXProject/src/view/RequestsView.java
+++ FinalFXProject/src/view/RequestsView.java
@@ -1,20 +1,9 @@
-package view;
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+package view;import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.border.EmptyBorder;
 
 import control.SQLQueries;
 import model.Hotel;
@@ -23,110 +12,116 @@ import utils.RequestType;
 
 public class RequestsView extends JFrame implements ActionListener {
 
-	private JFrame nextFrame;
-	private JPanel contentPane;
-	private JButton btnSend;
-	private JTextArea txtArea;
-	private JButton btnBack;
+    private JFrame nextFrame;
+    private JPanel contentPane;
+    private JButton btnSend;
+    private JTextArea txtArea;
+    private JButton btnBack;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					RequestsView frame = new RequestsView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    RequestsView frame = new RequestsView();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public RequestsView() {
-		super("Requests");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		initialize();
-		setVisible(true);
-	}
-	
-	public RequestsView(JFrame nextFrame) {
-		super("Requests");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		this.nextFrame=nextFrame;
-		initialize();
-		setVisible(true);
-	}
-	
-	public void initialize() {
-		contentPane = new JPanel();
-	    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	    contentPane.setLayout(new BorderLayout(0, 0));
+    public RequestsView() {
+        super("Requests");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(350, 250); // Adjusted size
+        setLocationRelativeTo(null); // Center the frame
+        initialize();
+        setVisible(true);
+    }
 
-	    JScrollPane scrollPane = new JScrollPane();
-	    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); 
-	    contentPane.add(scrollPane, BorderLayout.CENTER);
+    public RequestsView(JFrame nextFrame) {
+        super("Requests");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(350, 250); // Adjusted size
+        setLocationRelativeTo(null); // Center the frame
+        this.nextFrame = nextFrame;
+        initialize();
+        setVisible(true);
+    }
 
-	    JPanel panel = new JPanel();
-	    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-	    scrollPane.setViewportView(panel);
-	    setContentPane(contentPane);
+    public void initialize() {
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(10, 10, 10, 10)); // Adjusted border
+        contentPane.setLayout(new BorderLayout());
+  
 
-	    // Add title
-	    JLabel lblTitle = new JLabel("<html><h1>Requests</h1></html>");
-	    panel.add(lblTitle);
-	    
-	    JLabel lblDesc = new JLabel("If you have any additional requests, do not hesitate to send them through here.\n"
-	    		+ "If your requests are about housekeeping/laundry, any hotel services or room service, we ask that you "
-	    		+ "leave those requests in their respective pages.");
-	    panel.add(lblDesc);
-	    
-	    JLabel lblQuestion = new JLabel("What may we help you with?");
-	    panel.add(lblQuestion);
-	    
-	    JPanel textAreaPanel = new JPanel(new BorderLayout());
-	    txtArea = new JTextArea(3,10);
-	    //txtArea.setLineWrap(true);
-	    //txtArea.setWrapStyleWord(true);
-	    textAreaPanel.add(txtArea, BorderLayout.CENTER); // Add the JTextArea to the center of the panel
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Set horizontal scroll bar policy
+        contentPane.add(scrollPane, BorderLayout.CENTER);
 
-	    panel.add(textAreaPanel);
-	    
-	    btnSend = new JButton("Send Request");
-	    btnSend.addActionListener(this); 
-	    panel.add(btnSend);
-	    
-	    btnBack = new JButton("Back");
-	    btnBack.addActionListener(this); 
-	    contentPane.add(btnBack,BorderLayout.NORTH);
-	    
-	}
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        scrollPane.setViewportView(panel);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	    if (e.getSource() == btnSend) {
-	        Request request = new Request(Hotel.getRoomNumber(), Hotel.getClientID(), LocalDateTime.now(), 
-	                RequestType.OTHER, txtArea.getText());
-	        Hotel.getInstance().getRequests().add(request);
-	        boolean success = SQLQueries.insertDataIntoTblRequest(request);
-	        if (success) {
-	            JOptionPane.showMessageDialog(this, "Request sent successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
-	        } else {
-	            JOptionPane.showMessageDialog(this, "Failed to send request.", "Error", JOptionPane.ERROR_MESSAGE);
-	        }
-	    }
-	    if (e.getSource() == btnBack) {
-	        nextFrame.setVisible(true);
-	        this.setVisible(false);
-	    }
-	}
+        JLabel lblTitle = new JLabel("<html><h1>Requests</h1></html>");
+        lblTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(lblTitle);
 
+        JLabel lblDesc = new JLabel("<html>If you have any additional requests, do not hesitate to send them through here.<br>"
+                + "If your requests are about housekeeping/laundry, any hotel services, or room service,<br>"
+                + "we ask that you leave those requests in their respective pages.</html>");
+        lblDesc.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblDesc.setBorder(new EmptyBorder(10, 380, 20, 0)); // Adjusted border
+        panel.add(lblDesc);
 
+        JLabel lblQuestion = new JLabel("What may we help you with?");
+        lblQuestion.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblQuestion.setBorder(new EmptyBorder(10, 0, 10, 0)); // Adjusted border
+        panel.add(lblQuestion);
+
+        txtArea = new JTextArea(10, 30); // Adjusted size
+        txtArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtArea.setMaximumSize(new Dimension(500, 400)); // Adjusted width
+        panel.add(txtArea);
+
+        btnSend = new JButton("Send Request");
+        btnSend.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnSend.addActionListener(this);
+        panel.add(btnSend);
+
+        btnBack = new JButton("Back");
+        btnBack.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnBack.addActionListener(this);
+        contentPane.add(btnBack, BorderLayout.NORTH);
+
+        setContentPane(contentPane);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnSend) {
+            sendRequest();
+        }
+        if (e.getSource() == btnBack) {
+            navigateBack();
+        }
+    }
+
+    private void sendRequest() {
+        Request request = new Request(Hotel.getRoomNumber(), Hotel.getClientID(), LocalDateTime.now(),
+                RequestType.OTHER, txtArea.getText());
+        Hotel.getInstance().getRequests().add(request);
+        boolean success = SQLQueries.insertDataIntoTblRequest(request);
+        if (success) {
+            JOptionPane.showMessageDialog(this, "Request sent successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Failed to send request.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void navigateBack() {
+        nextFrame.setVisible(true);
+        this.setVisible(false);
+    }
 }
